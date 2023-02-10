0481a8db32ca8ec1a8f39b1dcea56871716d3fd4
Angga Gantira <angga.gantira@gmail.com>
2023-02-10 18:14:42 +0700

    Let's start working on Appointment Module using Vue 3 and Laravel | Part 23

---
+++ database/migrations/2023_02_10_084642_create_appointments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Client;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Client::class)->constrained();
+            $table->string('title');
+            $table->text('description');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointmens');
+    }
+};
