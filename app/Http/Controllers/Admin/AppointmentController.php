3343acd46f0205382c31fc194711276910a758d2
Angga Gantira <angga.gantira@gmail.com>
2023-03-19 13:50:48 +0700

    Appointment Form Validation using Vue 3, VeeValidate and Laravel | Part 29

--- app/Http/Controllers/Admin/AppointmentController.php
+++ app/Http/Controllers/Admin/AppointmentController.php
@@ -53,6 +53,11 @@ class AppointmentController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+        
         Appointment::create([
             'title' => $request->title,
             'client_id' => 1,
