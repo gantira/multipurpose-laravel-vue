77f7ac302307b57ddb9b120d2998094a97731267
Angga Gantira <angga.gantira@gmail.com>
2024-02-10 11:56:50 +0700

    How to Implement Dark Mode in the AdminLTE Theme Using Laravel, Vue 3, Pinia, and LocalStorage?

--- resources/js/stores/SettingStore.js
+++ resources/js/stores/SettingStore.js
@@ -1,18 +1,25 @@
 import axios from "axios";
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { useStorage } from "@vueuse/core";
 
 export const useSettingStore = defineStore('SettingStore', () => {
     const setting = ref({
         app_name: '',
     })
 
-    const getSetting = () => {
-        axios.get('/api/settings')
-        .then((response) => {
-            setting.value = response.data
-        })
+    const theme = useStorage('SettingStore:theme', ref('light'));
+
+    const changeTheme = () => {
+        theme.value = theme.value === 'light' ? 'dark' : 'light'
     }
 
-    return { setting, getSetting }
+    const getSetting = () => {
+        axios.get('/api/settings')
+            .then((response) => {
+                setting.value = response.data
+            })
+    }
+
+    return { setting, getSetting, theme, changeTheme }
 })
